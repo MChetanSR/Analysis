1fb6e319cb861e5345d146926ba4ed6afe33a05b
MChetanSR <sriram.phyiitg@gmail.com>
2020-07-06 08:05:34 +0800

    =added specFad in init

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .ShadowImage import ShadowImage
 from .fits import gaussian, gaussianFit, gaussian2D, gaussian2DFit, lorentzian, lorentzianFit
 from .sigma import sigmaRed, sigmaBlue
 from .numberOfAtoms import numAtomsRed, numAtomsBlue
-from .spectrum import spectroscopy
+from .spectrum import spectroscopy, spectroscopyFaddeva
